452effa38b086e1e7501d68814d7aa4dcea08464
Ashwin <yadgirkerashwin202@gmail.com>
2021-07-16 12:10:03 +0530

    added multiple tags

--- e2e/support/cucumber-hooks.ts
+++ e2e/support/cucumber-hooks.ts
@@ -3,18 +3,14 @@ import { browser } from 'protractor';
 
 // Default before anything else, open the app and clear previous state
 Before({ timeout: 20 * 5000 }, async () => {
-  await browser.executeScript('window.sessionStorage.clear();');
-  await browser.executeScript('window.localStorage.clear();');
   await browser.manage().deleteAllCookies();
   await browser.get(browser.baseUrl);
 });
 
 // Note: The After hooks are run in reverse order that they are defined in
 // If the scenario failed, include a screenshot of the last step.
-After({ timeout: 100 * 1000 }, async function(testCase) {
+After({ timeout: 100 * 1000 }, function(testCase) {
   const world = this;
-  await browser.executeScript('window.sessionStorage.clear();');
-  await browser.executeScript('window.localStorage.clear();');
   if (testCase.result.status !== 'passed' || true) {
     return browser.takeScreenshot().then((screenShot) => {
       world.attach(screenShot, 'image/png'); // screenShot is a base-64 encoded PNG
